4dd96436badf4e7d6299547c811409479f2b1251
ICT <ict@cap-jk-a008.kukkula.local>
2026-08-13 12:19:17 +0300

    calcci

--- src/calc.ts
+++ src/calc.ts
@@ -1,12 +1,34 @@
 
-function sum(): void {
+document.getElementById("+").addEventListener("click", (e: PointerEvent) => {
     const inputA: HTMLInputElement = document.getElementById("a") as HTMLInputElement
     const inputB: HTMLInputElement = document.getElementById("b") as HTMLInputElement
     
     const result: number = parseFloat(inputA.value) + Number(inputB.value)
     document.getElementById("result").innerHTML = result.toString()
-}
+})
+
+document.getElementById("-").addEventListener("click", (e: PointerEvent) => {
+    const inputA: HTMLInputElement = document.getElementById("a") as HTMLInputElement
+    const inputB: HTMLInputElement = document.getElementById("b") as HTMLInputElement
+    
+    const result: number = parseFloat(inputA.value) - Number(inputB.value)
+    document.getElementById("result").innerHTML = result.toString()
+})
+
+document.getElementById("*").addEventListener("click", (e: PointerEvent) => {
+    const inputA: HTMLInputElement = document.getElementById("a") as HTMLInputElement
+    const inputB: HTMLInputElement = document.getElementById("b") as HTMLInputElement
+    
+    const result: number = parseFloat(inputA.value) * Number(inputB.value)
+    document.getElementById("result").innerHTML = result.toString()
+})
+
+document.getElementById("/").addEventListener("click", (e: PointerEvent) => {
+    const inputA: HTMLInputElement = document.getElementById("a") as HTMLInputElement
+    const inputB: HTMLInputElement = document.getElementById("b") as HTMLInputElement
+    
+    const result: number = parseFloat(inputA.value) / Number(inputB.value)
+    document.getElementById("result").innerHTML = result.toString()
+})
 
 export {}
-
-window.sum = sum
