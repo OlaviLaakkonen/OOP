cfd3141f4f9943820cdfca2e1f3b33bff2b72dbf
Olavi Laakkonen <olavi.laakkonen@kukkula.local>
2026-08-17 09:40:17 +0300

    pretask 2 done

--- public/build/calc.js
+++ public/build/calc.js
@@ -1,9 +1,26 @@
-function sum() {
+document.getElementById("+").addEventListener("click", (e) => {
     const inputA = document.getElementById("a");
     const inputB = document.getElementById("b");
     const result = parseFloat(inputA.value) + Number(inputB.value);
     document.getElementById("result").innerHTML = result.toString();
-}
-window.sum = sum;
+});
+document.getElementById("-").addEventListener("click", (e) => {
+    const inputA = document.getElementById("a");
+    const inputB = document.getElementById("b");
+    const result = parseFloat(inputA.value) - Number(inputB.value);
+    document.getElementById("result").innerHTML = result.toString();
+});
+document.getElementById("*").addEventListener("click", (e) => {
+    const inputA = document.getElementById("a");
+    const inputB = document.getElementById("b");
+    const result = parseFloat(inputA.value) * Number(inputB.value);
+    document.getElementById("result").innerHTML = result.toString();
+});
+document.getElementById("/").addEventListener("click", (e) => {
+    const inputA = document.getElementById("a");
+    const inputB = document.getElementById("b");
+    const result = parseFloat(inputA.value) / Number(inputB.value);
+    document.getElementById("result").innerHTML = result.toString();
+});
 export {};
 //# sourceMappingURL=calc.js.map
--- src/calc.ts
+++ src/calc.ts
@@ -1,34 +1,34 @@
 
-document.getElementById("+").addEventListener("click", (e: PointerEvent) => {
+document.getElementById("+")!.addEventListener("click", (e: PointerEvent) => {
     const inputA: HTMLInputElement = document.getElementById("a") as HTMLInputElement
     const inputB: HTMLInputElement = document.getElementById("b") as HTMLInputElement
     
     const result: number = parseFloat(inputA.value) + Number(inputB.value)
-    document.getElementById("result").innerHTML = result.toString()
+    document.getElementById("result")!.innerHTML = result.toString()
 })
 
-document.getElementById("-").addEventListener("click", (e: PointerEvent) => {
+document.getElementById("-")!.addEventListener("click", (e: PointerEvent) => {
     const inputA: HTMLInputElement = document.getElementById("a") as HTMLInputElement
     const inputB: HTMLInputElement = document.getElementById("b") as HTMLInputElement
     
     const result: number = parseFloat(inputA.value) - Number(inputB.value)
-    document.getElementById("result").innerHTML = result.toString()
+    document.getElementById("result")!.innerHTML = result.toString()
 })
 
-document.getElementById("*").addEventListener("click", (e: PointerEvent) => {
+document.getElementById("*")!.addEventListener("click", (e: PointerEvent) => {
     const inputA: HTMLInputElement = document.getElementById("a") as HTMLInputElement
     const inputB: HTMLInputElement = document.getElementById("b") as HTMLInputElement
     
     const result: number = parseFloat(inputA.value) * Number(inputB.value)
-    document.getElementById("result").innerHTML = result.toString()
+    document.getElementById("result")!.innerHTML = result.toString()
 })
 
-document.getElementById("/").addEventListener("click", (e: PointerEvent) => {
+document.getElementById("/")!.addEventListener("click", (e: PointerEvent) => {
     const inputA: HTMLInputElement = document.getElementById("a") as HTMLInputElement
     const inputB: HTMLInputElement = document.getElementById("b") as HTMLInputElement
     
     const result: number = parseFloat(inputA.value) / Number(inputB.value)
-    document.getElementById("result").innerHTML = result.toString()
+    document.getElementById("result")!.innerHTML = result.toString()
 })
 
 export {}
