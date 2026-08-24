56dab38928bdde0c67b133aae04abeab25a6889c
Olavi Laakkonen <olavi.laakkonen@kukkula.local>
2026-08-24 14:00:50 +0300

    task 1.3 done

--- src/shapes-demo.ts
+++ src/shapes-demo.ts
@@ -5,43 +5,33 @@ const ctx: CanvasRenderingContext2D = canvas.getContext("2d")
 
 const xCenter: number = canvas.width / 2
 const yCenter: number = canvas.height / 2
- 
-const rectangle1 = {
+
+const rectangle = {
     x: xCenter - 200,
     y: yCenter - 100,
-    width: 500,
-    height: 350
-}
+    width: 400,
+    height: 200,
 
-const rectangle2 = {
-    x: xCenter - 120,
-    y: yCenter - 25,
-    width: 350,
-    height: 200
-}
+    draw: function() {
+        ctx.fillStyle = "#006A4E",
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+};
 
-const rectangle3 = {
-    x: xCenter - 120,
-    y: yCenter - -55,
-    width: 350,
-    height: 50
-}
+rectangle.draw()
 
-const rectangle4 = {
+const circle = {
     x: xCenter - 20,
-    y: yCenter - 25,
-    width: 50,
-    height: 200
+    y: yCenter - 0,
+    radius1: 70,
+    radius2: 0,
+
+    draw: function() {
+        ctx.fillStyle = "#DA291C"
+        ctx.beginPath();
+        ctx.arc(circle.x, circle.y, circle.radius1, circle.radius2, circle.radius1 * Math.PI)
+        ctx.fill();
+    }
 }
 
-ctx.fillStyle = "#002F6C"
-ctx.fillRect(rectangle1.x, rectangle1.y, rectangle1.width, rectangle1.height)
-
-ctx.fillStyle = "white"
-ctx.fillRect(rectangle2.x, rectangle2.y, rectangle2.width, rectangle2.height)
-
-ctx.fillStyle = "#002F6C"
-ctx.fillRect(rectangle3.x, rectangle3.y, rectangle3.width, rectangle3.height)
-
-ctx.fillStyle = "#002F6C"
-ctx.fillRect(rectangle4.x, rectangle4.y, rectangle4.width, rectangle4.height)
+circle.draw()
