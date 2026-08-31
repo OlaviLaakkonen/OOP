fdeabed3f8e58f6710ff9d5c0f9bde826c7fe9e4
Olavi Laakkonen <olavi.laakkonen@kukkula.local>
2026-08-31 13:10:59 +0300

    2.2 done

--- src/shapes.ts
+++ src/shapes.ts
@@ -4,41 +4,59 @@ const canvas: HTMLCanvasElement = document.getElementById("myCanvas") as HTMLCan
 const ctx: CanvasRenderingContext2D = canvas.getContext("2d")
 
 export class rectangle {
-    x: number;
-    y: number;
-    width: number;
-    height: number;
 
-    constructor(x = 0, y = 0, width = 0, height = 0) {
-        this.x = x;
-        this.y = y; 
-        this.width = width; 
-        this.height = height;
+    location: Point
+    size: Size
+
+    constructor(x: number, y: number, width: number, height: number) {
+        this.location = new Point(x, y)
+        this.size = new Size(width, height)
     }
 
     draw() {
         ctx.fillStyle = "#008a65ff"
-        ctx.fillRect(this.x, this.y, this.width, this.height)
+        ctx.fillRect(this.location.x, this.location.y, this.size.width, this.size.height)
     }
 }
 
 export class circle {
-    x: number;
-    y: number;
-    radius1: number;
-    radius2: number;
 
-    constructor(x = 0, y = 0, radius1 = 0, radius2 = 0) {
-        this.x = x;
-        this.y = y; 
-        this.radius1 = radius1; 
-        this.radius2 = radius2;
+    center: Point
+    radius: number
+
+    constructor(x: number, y: number, radius: number) {
+        this.center = new Point(x, y)
+        this.radius = radius
     }
 
     draw() {
         ctx.fillStyle = "#c00d00ff"
         ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius1, this.radius2, this.radius1 * Math.PI)
+        ctx.arc(this.center.x, this.center.y, this.radius, 0, 2 * Math.PI)
         ctx.fill();
     }
+}
+
+class Point {
+
+    x: number
+    y: number
+
+    constructor (x = 0, y = 0) {
+        this.x = x
+        this.y = y
+    }
+
+}
+
+class Size {
+
+    width: number
+    height: number
+
+    constructor (width = 0, height = 0) {
+        this.width = width
+        this.height = height
+    }
+
 }
