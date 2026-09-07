f5b3830babd43a54407879d59696e2f8088095bc
Olavi Laakkonen <olavi.laakkonen@kukkula.local>
2026-09-07 11:23:21 +0300

    2.7 done

--- src/shapes-viewer-demo.ts
+++ src/shapes-viewer-demo.ts
@@ -1,26 +1,28 @@
 import { ShapeViewer } from "./shape-viewer.js"
 import { circle, rectangle } from "./shapes.js"
 
-const shapeViewer: shapeViewer = new ShapeViewer(document.getElementById("myCanvas") as HTMLCanvasElement)
+const shapeViewer: ShapeViewer = new ShapeViewer(document.getElementById("myCanvas") as HTMLCanvasElement)
 
 shapeViewer.addShapes([
-    new rectangle(100, 200, 200, 300),
-    new rectangle(350, 200, 100, 100),
-    new rectangle(300, 400, 200, 100),
-    new rectangle(500, 200, 200, 300),
+    new rectangle(100, 200, 200, 300, "dodgerblue"),
+    new rectangle(350, 200, 100, 100, "dodgerblue"),
+    new rectangle(300, 400, 200, 100, "dodgerblue"),
+    new rectangle(500, 200, 200, 300, "dodgerblue"),
 
-    new circle(100, 200, 50),
-    new circle(700, 200, 50),
-    new circle(100, 500, 50),
-    new circle(700, 500, 50),
+    new circle(100, 200, 50, "white"),
+    new circle(700, 200, 50, "white"),
+    new circle(100, 500, 50, "white"),
+    new circle(700, 500, 50, "white"),
     
-    new circle(450, 200, 50),
-    new circle(350, 200, 50),
-    new circle(350, 300, 50),
-    new circle(450, 300, 50),
+    new circle(450, 200, 50, "white"),
+    new circle(350, 200, 50, "white"),
+    new circle(350, 300, 50, "white"),
+    new circle(450, 300, 50, "white"),
     
-    new circle(300, 200, 50),
-    new circle(500, 200, 50),
-    new circle(300, 400, 50),
-    new circle(500, 400, 50),
+    new circle(300, 200, 50, "white"),
+    new circle(500, 200, 50, "white"),
+    new circle(300, 400, 50, "white"),
+    new circle(500, 400, 50, "white"),
 ])
+
+console.log(shapeViewer.toString())
--- src/shapes.ts
+++ src/shapes.ts
@@ -2,7 +2,18 @@ const canvas = document.getElementById('myCanvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d')!;
 
 export abstract class Shape {
+
+    style: string
+    
+    constructor(style:string) {
+        this.style = style
+    }
+
     abstract draw(ctx: CanvasRenderingContext2D): void
+
+    toString(): string {
+        return `Shape with style ${this.style}`
+    }
 }
 
 export class rectangle extends Shape {
@@ -10,15 +21,19 @@ export class rectangle extends Shape {
     location: Point
     size: Size
 
-    constructor(x: number, y: number, width: number, height: number) {
-        super()
+    constructor(x: number, y: number, width: number, height: number, style: string) {
+        super(style)
         this.location = new Point(x, y)
         this.size = new Size(width, height)
     }
 
     draw() {
-        ctx.fillStyle = "#c5170aff"
+        ctx.fillStyle = this.style
         ctx.fillRect(this.location.x, this.location.y, this.size.width, this.size.height)
+    }
+
+    toString(): string {
+        return `Rectangle with location ${this.location}, size ${this.size}, ${super.toString()}`
     }
 }
 
@@ -27,17 +42,21 @@ export class circle extends Shape {
     center: Point
     radius: number
 
-    constructor(x: number, y: number, radius: number) {
-        super()
+    constructor(x: number, y: number, radius: number, style: string) {
+        super(style)
         this.center = new Point(x, y)
         this.radius = radius
     }
 
     draw() {
-        ctx.fillStyle = "#ffffffff"
+        ctx.fillStyle = this.style
         ctx.beginPath();
         ctx.arc(this.center.x, this.center.y, this.radius, 0, 2 * Math.PI)
         ctx.fill();
+    }
+
+    toString(): string {
+        return `Circle with location ${this.center}, radius ${this.radius}, ${super.toString()}`
     }
 }
 
@@ -51,6 +70,10 @@ class Point {
         this.y = y
     }
 
+    // fixes location [object Object]
+    toString(): string {
+        return `(${this.x}, ${this.y})`
+    }
 }
 
 class Size {
@@ -61,6 +84,11 @@ class Size {
     constructor (width = 0, height = 0) {
         this.width = width
         this.height = height
+    }
+
+    // fixes size [object Object]
+    toString(): string {
+        return `${this.width} x ${this.height}`
     }
 
 }
